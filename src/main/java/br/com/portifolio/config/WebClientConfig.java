3475c54da5ca7a5a0032bc68be49bcb5c8cbb2b0
AfrÃ¢nio Baroni <baroninet@gmail.com>
2026-06-23 15:00:00 -0300

    chore: replace Tomcat with Undertow and configure HikariCP pool

--- src/main/java/br/com/portifolio/config/WebClientConfig.java
+++ src/main/java/br/com/portifolio/config/WebClientConfig.java
@@ -1,14 +1,32 @@
 package br.com.portifolio.config;
 
+import io.netty.channel.ChannelOption;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 
 @Configuration
+@EnableConfigurationProperties(ExternalMembersProperties.class)
 public class WebClientConfig {
 
     @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+    public WebClient memberExternalWebClient(ExternalMembersProperties props) {
+        ConnectionProvider provider = ConnectionProvider.builder("member-external-pool")
+                .maxConnections(props.getMaxConnections())
+                .maxIdleTime(props.getMaxIdleTime())
+                .build();
+
+        HttpClient httpClient = HttpClient.create(provider)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, (int) props.getConnectTimeout().toMillis())
+                .responseTimeout(props.getReadTimeout());
+
+        return WebClient.builder()
+                .baseUrl(props.getBaseUrl())
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
 }
